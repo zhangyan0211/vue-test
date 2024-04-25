a027883c127b1e41c91c4a80cb12d0eecd5b8146
zhangyan <zhangyan3474@163.com>
2024-04-25 15:08:01 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <div id="app-1">
+      <p>{{msg}}</p>
+      <a href="./components/work/workHome.vue" target="_blank" rel="noopener">点击我跳转到</a>
+      <router-link to="/components/work/workHome.vue"><a>跳转</a></router-link>
+    </div>
+      <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
 
 export default {
+
   name: 'App',
   components: {
     HelloWorld
   }
 }
+
+new Vue({
+  el : '#app-1',
+  data: {
+    msg : 'hello zhangyan welcome'
+  }
+})
 </script>
 
 <style>
---
+++ src/components/work/workHome.vue
@@ -0,0 +1,32 @@
+<!--  -->
+<template>
+<div id='12'>
+    <p>123456</p>
+</div>
+</template>
+
+<script>
+
+export default {
+components: {},
+data() {
+return {
+
+}
+},
+computed: {},
+watch: {},
+methods: {
+
+},
+created() {
+
+},
+mounted() {
+
+},
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
